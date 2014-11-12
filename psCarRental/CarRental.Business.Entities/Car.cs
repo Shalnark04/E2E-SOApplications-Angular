9f1364a4651b3248464c2fd50ee4f41d72627d1d
Jon Bachelor <jon_bachelor@me.com>
2014-11-11 22:32:47 -0800

    Checking in accidentally 'untracked' files.

---
+++ psCarRental/CarRental.Business.Entities/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Common.Contracts;
+using Core.Common.Core;
+
+namespace CarRental.Business.Entities
+{
+    [DataContract]
+    public class Car : EntityBase, IIdentifiableEntity
+    {
+        [DataMember]
+        public int CarId { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Color { get; set; }
+
+        [DataMember]
+        public int Year { get; set; }
+
+        [DataMember]
+        public decimal RentalPrice { get; set; }
+
+        [DataMember]
+        public bool CurrentlyRented { get; set; }
+
+        public int EntityId
+        {
+            get { return CarId; }
+            set { CarId = value; }
+        }
+    }
+}
---
+++ psCarRental/CarRental.Client.Entities/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Client.Entities
+{
+    public class Car
+    {
+        private int _CarId;
+        private string _Description;
+        private string _Color;
+        private int _Year;
+        private decimal _RentalPrice;
+        private bool _CurrentlyRented;
+
+        public int CarId
+        {
+            get { return _CarId; }
+            set { _CarId = value; }
+        }
+
+        public string Description
+        {
+            get { return _Description; }
+            set { _Description = value; }
+        }
+
+        public string Color
+        {
+            get { return _Color; }
+            set { _Color = value; }
+        }
+
+        public int Year
+        {
+            get { return _Year; }
+            set { _Year = value; }
+        }
+
+        public decimal RentalPrice
+        {
+            get { return _RentalPrice; }
+            set { _RentalPrice = value; }
+        }
+
+        public bool CurrentlyRented
+        {
+            get { return _CurrentlyRented; }
+            set { _CurrentlyRented = value; }
+        }
+    }
+}
